805d8721484a2aa6371ab7cda0ec3cf8d26c2bbd
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-03-10 21:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor =
+    document.body.style.backgroundColor === 'lightblue'
+      ? '#f8f8f8'
+      : 'lightblue';
+});
